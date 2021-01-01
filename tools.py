9a29d3e5f18ed14405f92fc11280621b466c5caf
Daniel <daniiomir@yandex.ru>
2021-01-01 17:52:28 +0300

    Baseline finished

--- tools.py
+++ tools.py
@@ -14,6 +14,8 @@ class LinearRegression:
         self.loss = loss
         self.lr = learning_rate
         self.verbose = verbose
+        self.data_mean = 0.0
+        self.data_std = 0.0
 
         if self.lr > 1:
             self.lr = 0.1
@@ -27,12 +29,13 @@ class LinearRegression:
         elif self.loss == 'mae':
             self.loss = MAELoss()
         else:
-            print(f'Error! Selected wrong loss function: {self.loss}')
+            print('Error! Selected wrong loss function: {}'.format(self.loss))
             quit()
 
     def fit(self, data, target):
         delta_loss = 1
         epoch = 1
+        data = self.scale_data(data)
         tmp_loss = self.loss(target, self.tmp_predict(data))
         while abs(delta_loss) > 0.000001:
             self.tmp_theta0 -= self.lr * (self.tmp_predict(data) - target).mean()
@@ -40,18 +43,28 @@ class LinearRegression:
             new_tmp_loss = self.loss(target, self.tmp_predict(data))
             delta_loss = new_tmp_loss - tmp_loss
             tmp_loss = new_tmp_loss
-            print(f'Epoch: {epoch}, loss: {tmp_loss}')
+            print('Epoch: {}, loss: {}'.format(epoch, tmp_loss))
             self.theta0 = self.tmp_theta0
             self.theta1 = self.tmp_theta1
             epoch += 1
         if self.verbose:
-            print(f'Fitted in {epoch}, theta0 - {self.theta0}, theta1 - {self.theta1}')
+            print('Fitted in {}, theta0 - {}, theta1 - {}'.format(epoch, self.theta0, self.theta1))
 
     def tmp_predict(self, data):
         return (self.tmp_theta1 * data) + self.tmp_theta0
 
     def predict(self, value):
-        return (self.theta1 * float(value)) + self.theta0
+        if isinstance(value, int):
+            if value > 0:
+                return int((self.theta1 * self.scale_data(value, False)) + self.theta0)
+        print('Error! Wrong input value - {}\nValue should be number bigger than zero.'.format(value))
+        quit()
+
+    def scale_data(self, data, is_train=True):
+        if is_train:
+            self.data_mean = np.mean(data)
+            self.data_std = np.std(data, ddof=1)
+        return (data - self.data_mean) / self.data_std
 
 
 class MSELoss:
@@ -110,7 +123,3 @@ def read_data(path):
             km = np.append(km, int(row['km']))
             price = np.append(price, int(row['price']))
     return km, price
-
-
-def standart_scale(data):
-    return (data - np.mean(data)) / np.std(data, ddof=1)
