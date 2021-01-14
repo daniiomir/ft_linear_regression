05ad90e6f14bfd8a1a6b89140a9ba0fe5c14b80f
Dan <daniiomir@yandex.ru>
2021-01-14 15:00:25 +0300

    Finished

--- tools.py
+++ tools.py
@@ -3,17 +3,17 @@ import random
 import pickle
 import argparse
 import numpy as np
+from typing import Union
 
 
 class LinearRegression:
-    def __init__(self, learning_rate, loss, verbose):
+    def __init__(self, learning_rate: float, verbose: int):
+        self.lr = learning_rate
+        self.verbose = verbose
         self.theta0 = 0.0
         self.theta1 = 0.0
         self.tmp_theta0 = random.random()
         self.tmp_theta1 = random.random()
-        self.loss = loss
-        self.lr = learning_rate
-        self.verbose = verbose
         self.data_mean = 0.0
         self.data_std = 0.0
 
@@ -24,15 +24,7 @@ class LinearRegression:
             self.lr = 0.1
             print('Warning! Too small learning rate. It was set to 0.1.')
 
-        if self.loss == 'mse':
-            self.loss = MSELoss()
-        elif self.loss == 'mae':
-            self.loss = MAELoss()
-        else:
-            print('Error! Selected wrong loss function: {}'.format(self.loss))
-            quit()
-
-    def fit(self, data, target):
+    def fit(self, data: np.ndarray, target: np.ndarray):
         delta_loss = 1
         epoch = 1
         data = self.scale_data(data)
@@ -50,45 +42,27 @@ class LinearRegression:
         if self.verbose:
             print('Fitted in {}, theta0 - {}, theta1 - {}'.format(epoch, self.theta0, self.theta1))
 
-    def tmp_predict(self, data):
+    def loss(self, real: np.ndarray, pred: np.ndarray) -> float:
+        return ((pred - real) ** 2).mean()
+
+    def tmp_predict(self, data: np.ndarray):
         return (self.tmp_theta1 * data) + self.tmp_theta0
 
-    def predict(self, value):
-        if isinstance(value, (int, float)):
-            if value > 0:
-                return int((self.theta1 * self.scale_data(value, False)) + self.theta0)
-        print('Error! Wrong input value - {}\nValue should be number bigger than zero.'.format(value))
-        quit()
+    def predict(self, value: Union[int, float]):
+        if value > 0:
+            return int((self.theta1 * self.scale_data(value, False)) + self.theta0)
+        raise Exception('Error! Wrong input value - {}. Value should be number bigger than zero.'.format(value))
 
-    def scale_data(self, data, is_train=True):
+    def scale_data(self, data: Union[np.ndarray, int, float], is_train: bool = True):
         if is_train:
             self.data_mean = np.mean(data)
             self.data_std = np.std(data, ddof=1)
         return (data - self.data_mean) / self.data_std
 
 
-class MSELoss:
-    def __call__(self, real, pred):
-        if isinstance(real, np.ndarray) and isinstance(pred, np.ndarray):
-            return ((pred - real) ** 2).mean()
-        else:
-            print('Error! Targets are not in numpy array!')
-            quit()
-
-
-class MAELoss:
-    def __call__(self, real, pred):
-        if isinstance(real, np.ndarray) and isinstance(pred, np.ndarray):
-            return (np.abs(pred - real)).mean()
-        else:
-            print('Error! Targets are not in numpy array!')
-            quit()
-
-
 def parse_args_fit():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', default='data.csv')
-    parser.add_argument('--loss', default='mse')
     parser.add_argument('--learning_rate', default=0.1, type=float)
     parser.add_argument('--save_path', default='model.pkl')
     parser.add_argument('--verbose', default=1, type=int)
@@ -112,18 +86,18 @@ def parse_args_visualize():
     return args.__dict__
 
 
-def save_model(model, path):
+def save_model(model: object, path: str):
     with open(path, 'wb') as f:
         pickle.dump(model, f, protocol=4)
 
 
-def load_model(path):
+def load_model(path: str):
     with open(path, 'rb') as f:
         model = pickle.load(f)
     return model
 
 
-def read_data(path):
+def read_data(path: str):
     km = np.array([])
     price = np.array([])
     with open(path, 'r') as f:
--- visualize.py
+++ visualize.py
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     plt.plot(reg_x, reg_y, color='red', label='Regression')
     plt.legend(loc='best')
     plt.savefig('plot.png')
+    print('Plot saved to plot.png')
